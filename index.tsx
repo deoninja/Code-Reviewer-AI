763479c39bb43d3e552e65ce0a2fd73dc4d10f79
deo <deotri4@gmail.com>
2025-09-17 12:36:17 +0800

    file structure

--- index.tsx
+++ index.tsx
@@ -1,12 +1,11 @@
-
 import './src/index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './src/App';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
+  throw new Error('Could not find root element to mount to');
 }
 
 const root = ReactDOM.createRoot(rootElement);
